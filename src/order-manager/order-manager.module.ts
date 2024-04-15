3e96c44da27067dc298407ddf959d0425701860b
Daniil Troshin <oxytocc@gmail.com>
2024-04-15 22:07:57 +0700

    fix2

--- src/order-manager/order-manager.module.ts
+++ src/order-manager/order-manager.module.ts
@@ -3,10 +3,12 @@ import { OrderManagerService } from './order-manager.service';
 import { OrderManagerController } from './order-manager.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entity/order.entity';
+import { Client } from 'src/client-manager/entity/client.entity';
+import { Part } from 'src/parts-manager/entity/part.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Order])
+    TypeOrmModule.forFeature([Order, Client, Part])
   ],
   controllers: [OrderManagerController],
   providers: [OrderManagerService],
