90c3319d36d4546e390b923ed860dcea91a7916d
Daniil Troshin <oxytocc@gmail.com>
2024-10-13 17:02:23 +0700

    update create user dto

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -14,24 +14,20 @@ import { Role } from 'src/roles/role.enum';
 const passwordRegEx =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,20}$/;
 
+const phoneRegEx = /^8(?:\d{3}){2}(?:\d{2}){2}$/
+
 export class CreateUserDto {
   @ApiProperty()
-  @IsString()
-  @MinLength(2, { message: 'Name must have atleast 2 characters.' })
   @IsNotEmpty()
-  name: string;
-
-  @ApiProperty()
-  @IsNotEmpty()
-  @MinLength(3, { message: 'Username must have atleast 3 characters.' })
+  @MinLength(3, { message: 'Имя пользователя дожно иметь минимум 3 символа.' })
   @IsAlphanumeric(null, {
-    message: 'Username does not allow other than alpha numeric chars.',
+    message: 'Имя пользователя дожен состоять из английских букв и цифр.',
   })
   username: string;
 
   @ApiProperty()
   @IsNotEmpty()
-  @IsEmail(null, { message: 'Please provide valid Email.' })
+  @IsEmail(null, { message: 'Введите корректный email' })
   email: string;
 
   @ApiProperty()
@@ -46,15 +42,37 @@ export class CreateUserDto {
   @ApiProperty()
   @IsNotEmpty()
   @Matches(passwordRegEx, {
-    message: `Password must contain Minimum 8 and maximum 20 characters, 
-    at least one uppercase letter, 
-    one lowercase letter, 
-    one number and 
-    one special character`,
+    message: `Паролт должен содержать минимум 8 и максимум 20 символов, 
+    как минимум одну заглавную букву, 
+    одну строчную, 
+    одно число и 
+    один специальный символ`,
   })
   password: string;
 
   @ApiProperty()
   @IsEnum(Role)
   role: Role;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2, { message: 'Имя должно иметь минимум 2 символа.' })
+    firstName: string;
+  
+  @ApiProperty()
+  @IsString()
+  @MinLength(2, { message: 'Фамилия должна иметь минимум 2 символа.' })
+    secondName: string;
+
+  @ApiProperty()
+  @IsString()
+  @MinLength(2, { message: 'Отчество должно иметь минимум 2 символа.' })
+    thirdName: string;
+
+  @ApiProperty()
+  @Matches(phoneRegEx, {
+    message: `Номер телефона должен быть введён в формате 81234567890`,
+  })
+    phoneNumber: string;
 }
