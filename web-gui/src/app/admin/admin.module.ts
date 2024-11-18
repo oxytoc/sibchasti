f4c8c6250791833c482f6b1e8aaf59f91b94dca0
Daniil Troshin <oxytocc@gmail.com>
2024-11-18 19:31:42 +0700

    add admin guard

--- web-gui/src/app/admin/admin.module.ts
+++ web-gui/src/app/admin/admin.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from '../guards/auth.guard';
 import { AdminComponent } from './admin.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthGuard } from '../share/guards/auth.guard';
+import { AdminGuard } from '../share/guards/admin.guard';
 
 
 const AUTH_ROUTES = [
-  { path: 'parts', loadChildren: () => import('./parts/parts.module').then(m => m.PartsModule) },
+  { path: 'parts', loadChildren: () => import('./parts/parts.module').then(m => m.AdminPartsModule) },
   { path: 'clients', loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule) },
   { path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) },
   { path: 'popular-parts', loadChildren: () => import('./analyze-parts/analyze-parts.module').then(m => m.AnalyzePartsModule) },
@@ -17,7 +18,7 @@ const AUTH_ROUTES = [
 export const routes: Routes = [
   {
     path: '',
-    canActivate: [AuthGuard],
+    canActivate: [AuthGuard, AdminGuard],
     component: AdminComponent,
     children: AUTH_ROUTES,
     runGuardsAndResolvers: 'always',
